3901448a42aab5a480afecfc689537c36008ceaa
a20222366-code <a20222366@pucp.edu.pe>
2026-08-27 17:50:45 -0500

    Parte1 de la busqueda de mascotas

--- src/main/java/com/example/Lab2/controller/MascotaController.java
+++ src/main/java/com/example/Lab2/controller/MascotaController.java
@@ -5,6 +5,7 @@ import com.example.Lab2.repository.MascotaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -18,12 +19,43 @@ public class MascotaController {
     }
 
     @GetMapping("/mascotas")
-    public String listarMascotas(Model model) {
+    public String listarMascotas(
+            @RequestParam(required = false) String criterio,
+            @RequestParam(required = false) String valor,
+            Model model) {
 
-        //Suficiente para el listado
-        List<Mascota> mascotas = mascotaRepository.findAll();
+        List<Mascota> mascotas;
+
+        if (criterio == null || valor == null || valor.trim().isEmpty()) {
+
+            mascotas = mascotaRepository.findAll();
+
+        } else {
+
+            switch (criterio) {
+
+                case "nombre":
+                    mascotas = mascotaRepository.findByNombreContainingIgnoreCase(valor);
+                    break;
+
+                case "especie":
+                    mascotas = mascotaRepository.findByEspecieContainingIgnoreCase(valor);
+                    break;
+
+                case "estado":
+                    Boolean estado = valor.equalsIgnoreCase("activo");
+                    mascotas = mascotaRepository.findByEstado(estado);
+                    break;
+
+                default:
+                    mascotas = mascotaRepository.findAll();
+                    break;
+            }
+        }
 
         model.addAttribute("mascotas", mascotas);
+        model.addAttribute("criterio", criterio);
+        model.addAttribute("valor", valor);
 
         return "mascotas";
     }
--- src/main/java/com/example/Lab2/repository/MascotaRepository.java
+++ src/main/java/com/example/Lab2/repository/MascotaRepository.java
@@ -3,5 +3,14 @@ package com.example.Lab2.repository;
 import com.example.Lab2.entity.Mascota;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MascotaRepository extends JpaRepository<Mascota, Long> {
+
+    //Se realiza para encontrar las busquedas dependiendo de lo que ponga el usuario
+    List<Mascota> findByNombreContainingIgnoreCase(String nombre);
+    List<Mascota> findByEspecieContainingIgnoreCase(String especie);
+    List<Mascota> findByEstado(Boolean estado);
+
+
 }
